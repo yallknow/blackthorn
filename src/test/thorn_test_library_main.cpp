e6d2ad6e0017210762389ef3162a24981772ab16
yallknow <ignites_you@protonmail.com>
2023-02-05 00:26:11 +0300

    [UPD] (test) Added log builder test case

--- src/test/thorn_test_library_main.cpp
+++ src/test/thorn_test_library_main.cpp
@@ -3,13 +3,45 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include "../library/abstract/thorn_library_abstract_runnable.hpp"
+#include "../library/thorn_library_log_builder.hpp"
 #include "../library/thorn_library_logger.hpp"
 #include "../library/thorn_library_preprocessor.hpp"
 #include "../library/thorn_library_thread_pool.hpp"
 #include "thorn_test_fixture.hpp"
 
+BOOST_AUTO_TEST_CASE(thorn_test_library_test_case_log_builder) {
+  thorn::library::log_builder::msf_set_log_directory("log/test/log_builder/");
+
+  // NOTE: Log_builder is uninitialized from the start
+  BOOST_CHECK(!thorn::library::log_builder::msf_is_initialized());
+
+  // NOTE: msf_init() call on an uninitialized log_builder
+  thorn::library::log_builder::msf_init();
+  BOOST_CHECK(thorn::library::log_builder::msf_is_initialized());
+
+  // NOTE: msf_init() call on an initialized log_builder
+  thorn::library::log_builder::msf_init();
+  BOOST_CHECK(thorn::library::log_builder::msf_is_initialized());
+
+  // NOTE: msf_close() call on an initialized log_builder
+  thorn::library::log_builder::msf_destroy();
+  BOOST_CHECK(!thorn::library::log_builder::msf_is_initialized());
+
+  // NOTE: msf_close() call on an uninitialized log_builder
+  thorn::library::log_builder::msf_destroy();
+  BOOST_CHECK(!thorn::library::log_builder::msf_is_initialized());
+
+  // NOTE: Re-initializing a log_builder
+  thorn::library::log_builder::msf_init();
+  BOOST_CHECK(thorn::library::log_builder::msf_is_initialized());
+
+  // NOTE: Stopping a re-initialized log_builder
+  thorn::library::log_builder::msf_destroy();
+  BOOST_CHECK(!thorn::library::log_builder::msf_is_initialized());
+}
+
 BOOST_AUTO_TEST_CASE(thorn_test_library_test_case_logger) {
-  thorn::library::log_builder::msf_set_log_directory("log/test/logger/");
+  thorn::library::logger::msf_set_log_directory("log/test/logger/");
 
   // NOTE: Logger is uninitialized from the start
   BOOST_CHECK(!thorn::library::logger::msf_is_initialized());
