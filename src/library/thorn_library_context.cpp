c8442b70ec5f86864e89e1550e9377d7c33b1ced
yallknow <ignites_you@protonmail.com>
2023-02-06 22:47:51 +0300

    [NEW] (library) Added context class

---
+++ src/library/thorn_library_context.cpp
@@ -0,0 +1,61 @@
+#include "thorn_library_context.hpp"
+
+#include "thorn_library_preprocessor.hpp"
+
+thorn::library::context::context(const std::uint32_t pc_ThreadPoolSize) noexcept
+    : mv_FocusedThreadPool{[this]() -> void {
+                             if (this->mv_Context.stopped()) {
+                               this->mv_Context.restart();
+                             }
+
+                             this->mv_Context.run();
+                           },
+                           pc_ThreadPoolSize} {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+}
+
+thorn::library::context::~context() noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  this->mf_stop();
+}
+
+boost::asio::io_context& thorn::library::context::mf_get_context() noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  return this->mv_Context;
+}
+
+bool thorn::library::context::mpf_inner_run() noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  this->mv_OptionalWorkGuard.emplace(this->mv_Context.get_executor());
+
+  this->mv_FocusedThreadPool.mf_run();
+
+  if (!this->mv_FocusedThreadPool.mf_is_running()) {
+    _THORN_LIBRARY_LOG_ERROR_("Unable to run focused thread pool!");
+
+    return false;
+  }
+
+  return true;
+}
+
+bool thorn::library::context::mpf_inner_stop() noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  this->mv_OptionalWorkGuard.reset();
+
+  this->mv_Context.stop();
+
+  this->mv_FocusedThreadPool.mf_stop();
+
+  if (this->mv_FocusedThreadPool.mf_is_running()) {
+    _THORN_LIBRARY_LOG_ERROR_("Unable to stop focused thread pool!");
+
+    return false;
+  }
+
+  return true;
+}
---
+++ src/library/thorn_library_context.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#ifndef _THORN_LIBRARY_CONTEXT_
+#define _THORN_LIBRARY_CONTEXT_
+
+#include <boost/asio.hpp>
+#include <cstdint>
+#include <optional>
+#include <thread>
+
+#include "abstract/thorn_library_abstract_runnable.hpp"
+#include "thorn_library_focused_thread_pool.hpp"
+
+namespace thorn {
+namespace library {
+
+class context final : public abstract::runnable {
+ public:
+  explicit context(const std::uint32_t pc_ThreadPoolSize =
+                       std::thread::hardware_concurrency()) noexcept;
+  /* virtual */ ~context() noexcept override;
+
+  boost::asio::io_context& mf_get_context() noexcept;
+
+ private:
+  bool mpf_inner_run() noexcept override;
+  bool mpf_inner_stop() noexcept override;
+
+ private:
+  boost::asio::io_context mv_Context{};
+
+  std::optional<
+      boost::asio::executor_work_guard<boost::asio::io_context::executor_type>>
+      mv_OptionalWorkGuard{std::nullopt};
+
+  focused_thread_pool mv_FocusedThreadPool;
+
+ public:
+  explicit context(const context& pcl_Other) noexcept = delete;
+  explicit context(context&& pc_Other) noexcept = delete;
+
+  context& operator=(const context& pcl_Other) noexcept = delete;
+  context& operator=(context&& pc_Other) noexcept = delete;
+};
+
+}  // namespace library
+}  // namespace thorn
+
+#endif  // !_THORN_LIBRARY_CONTEXT_
