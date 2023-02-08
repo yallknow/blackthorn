991c316c093ff1f93005d76c2a48eada5f4061c3
yallknow <ignites_you@protonmail.com>
2023-02-08 21:40:58 +0300

    [NEW] (library) Added tcp connector class

---
+++ src/library/tcp/thorn_library_tcp_connector.cpp
@@ -0,0 +1,62 @@
+#include "thorn_library_tcp_connector.hpp"
+
+#include <boost/system/error_code.hpp>
+
+#include "../thorn_library_preprocessor.hpp"
+
+thorn::library::tcp::connector::connector(boost::asio::io_context& pl_Context,
+                                          const std::string_view pc_Address,
+                                          const std::uint16_t pc_Port) noexcept
+    : ml_Context{pl_Context}, mv_Address{pc_Address}, mv_Port{pc_Port} {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+}
+
+thorn::library::tcp::connector::~connector() noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  this->mf_stop();
+}
+
+void thorn::library::tcp::connector::mf_set_address(
+    const std::string_view pc_Address) noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  this->mv_Address = pc_Address;
+}
+
+void thorn::library::tcp::connector::mf_set_port(
+    const std::uint16_t pc_Port) noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  this->mv_Port = pc_Port;
+}
+
+bool thorn::library::tcp::connector::mpf_inner_run() noexcept {
+  _THORN_LIBRARY_LOG_FUNCTION_CALL_();
+
+  boost::system::error_code lv_ErrorCode{};
+  const boost::asio::ip::tcp::endpoint lc_Endpoint{
+      boost::asio::ip::make_address(this->mv_Address, lv_ErrorCode),
+      this->mv_Port};
+
+  if (lv_ErrorCode) {
+    _THORN_LIBRARY_LOG_ERROR_CODE_("Can't create endpoint!", lv_ErrorCode);
+
+    return false;
+  }
+
+  _THORN_LIBRARY_LOG_INFO_("Endpoint created.");
+
+  this->mv_OptionalSocket.emplace(this->ml_Context);
+  this->mv_OptionalSocket->connect(lc_Endpoint, lv_ErrorCode);
+
+  if (lv_ErrorCode) {
+    _THORN_LIBRARY_LOG_ERROR_CODE_("Can't connect!", lv_ErrorCode);
+
+    return false;
+  }
+
+  _THORN_LIBRARY_LOG_INFO_("Connection established.");
+
+  return true;
+}
---
+++ src/library/tcp/thorn_library_tcp_connector.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#ifndef _THORN_LIBRARY_TCP_CONNECTOR_
+#define _THORN_LIBRARY_TCP_CONNECTOR_
+
+#include <boost/asio.hpp>
+#include <cstdint>
+#include <string>
+#include <string_view>
+
+#include "abstract/thorn_library_tcp_abstract_socket_holder.hpp"
+
+namespace thorn {
+namespace library {
+namespace tcp {
+
+class connector final : public abstract::socket_holder {
+ public:
+  explicit connector(boost::asio::io_context& pl_Context,
+                     const std::string_view pc_Address,
+                     const std::uint16_t pc_Port) noexcept;
+  /* virtual */ ~connector() noexcept override;
+
+ public:
+  void mf_set_address(const std::string_view pc_Address) noexcept;
+  void mf_set_port(const std::uint16_t pc_Port) noexcept;
+
+ private:
+  bool mpf_inner_run() noexcept override;
+
+ private:
+  boost::asio::io_context& ml_Context;
+
+ private:
+  std::string mv_Address;
+  std::uint16_t mv_Port;
+
+ public:
+  explicit connector(const connector& pcl_Other) noexcept = delete;
+  explicit connector(connector&& pr_Other) noexcept = delete;
+
+ public:
+  connector& operator=(const connector& pcl_Other) noexcept = delete;
+  connector& operator=(connector&& pr_Other) noexcept = delete;
+};
+
+}  // namespace tcp
+}  // namespace library
+}  // namespace thorn
+
+#endif  // !_THORN_LIBRARY_TCP_CONNECTOR_
