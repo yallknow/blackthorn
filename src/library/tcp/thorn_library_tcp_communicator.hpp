7a68fba4b54d2a9737753e6a9bab1496f81443c5
yallknow <ignites_you@protonmail.com>
2023-02-26 02:49:58 +0300

    [UPD] (library) Updated library classes namespace

--- src/library/tcp/thorn_library_tcp_communicator.hpp
+++ src/library/tcp/thorn_library_tcp_communicator.hpp
@@ -25,19 +25,21 @@ class communicator final : public abstract::socket_holder {
   /* virtual */ ~communicator() noexcept override;
 
  public:
-  void mf_push_back(const std::shared_ptr<message> pcp_Message) noexcept;
-  void mf_push_front(const std::shared_ptr<message> pcp_Message) noexcept;
+  void mf_push_back(
+      const std::shared_ptr<thorn::library::message> pcp_Message) noexcept;
+  void mf_push_front(
+      const std::shared_ptr<thorn::library::message> pcp_Message) noexcept;
 
-  std::shared_ptr<message> mf_pop_back() noexcept;
-  std::shared_ptr<message> mf_pop_front() noexcept;
+  std::shared_ptr<thorn::library::message> mf_pop_back() noexcept;
+  std::shared_ptr<thorn::library::message> mf_pop_front() noexcept;
 
   // NOTE: Communicator should only accept open sockets
   void mf_set_socket(boost::asio::ip::tcp::socket&& pr_Socket) noexcept;
 
  private:
   // NOTE: We read the body if a message header is present
-  void mf_read(const std::shared_ptr<message_header> pcp_MessageHeader =
-                   nullptr) noexcept;
+  void mf_read(const std::shared_ptr<thorn::library::message_header>
+                   pcp_MessageHeader = nullptr) noexcept;
   void mf_write() noexcept;
 
  private:
@@ -48,8 +50,8 @@ class communicator final : public abstract::socket_holder {
   boost::asio::io_context::strand mv_ReadStrand;
   boost::asio::io_context::strand mv_WriteStrand;
 
-  safe_deque<std::shared_ptr<message>> mv_ReadDeque{};
-  safe_deque<std::shared_ptr<message>> mv_WriteDeque{};
+  safe_deque<std::shared_ptr<thorn::library::message>> mv_ReadDeque{};
+  safe_deque<std::shared_ptr<thorn::library::message>> mv_WriteDeque{};
 
  public:
   explicit communicator(const communicator& pcl_Other) noexcept = delete;
