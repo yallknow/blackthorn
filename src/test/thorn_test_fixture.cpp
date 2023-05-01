907dbf0a4b6918f3e86f5440b1a5cf1e570d2e59
yallknow <ignites_you@protonmail.com>
2023-05-01 14:51:32 +0300

    [UPD] (test) Updated fixture class

--- src/test/thorn_test_fixture.cpp
+++ src/test/thorn_test_fixture.cpp
@@ -1,12 +1,27 @@
 #include "thorn_test_fixture.hpp"
 
 #include "../library/thorn_library_log_builder.hpp"
+#include "../library/thorn_library_logger.hpp"
+#include "../library/thorn_library_scope_profiler.hpp"
 
 thorn::test::fixture::fixture() noexcept {
-  thorn::library::log_builder::msf_set_log_directory("log/test/");
+  thorn::library::logger::msf_set_log_directory("log/test/");
+  thorn::library::logger::msf_init();
+
+  if (!thorn::library::logger::msf_is_initialized()) {
+    return;
+  }
+
+  thorn::library::scope_profiler::msf_init();
   thorn::library::log_builder::msf_init();
 }
 
 thorn::test::fixture::~fixture() noexcept {
+  if (!thorn::library::logger::msf_is_initialized()) {
+    return;
+  }
+
+  thorn::library::scope_profiler::msf_destroy();
   thorn::library::log_builder::msf_destroy();
+  thorn::library::logger::msf_destroy();
 }
